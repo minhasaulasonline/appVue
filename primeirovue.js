5c0a3bcd4542fbf1ca9cc06f8373ec25294fa187
minhasaulasonline <57022268+minhasaulasonline@users.noreply.github.com>
2019-11-02 17:58:10 -0300

    Update primeirovue.js

--- primeirovue.js
+++ primeirovue.js
@@ -1,13 +1,79 @@
+var comp2c = "fui criado no scopo global";
+
+ Vue.directive('my-rotate', function(el) {
+     var angle = 0;
+     teste = false
+     color = 'blue';
+     el.addEventListener('click', function() {
+         teste ? color = "blue" : color = "red";
+         teste = !teste
+         angle += 90;
+         el.style.transform = ` rotateZ(${angle}deg) `;
+         el.style.backgroundColor = color;
+     })
+
+ })
+
  Vue.component("my-component", {
-     template: `<li>{{valor}}</li>`,
+     template: `<div>
+              <slot>Padrão</slot>
+              <li>{{valor}}</li>
+                       
+              </div>`,
+
      props: ['chave',
          'valor'
      ],
+     data: function() {
+         return {
+             teste4b: [
+                 { value: 'abacaxi' },
+                 { value: 'banana' },
+                 { value: 'coco' },
+                 { value: 'damascom' }
+             ]
+         }
+     }
+ })
+
+ Vue.component("my-comp2", {
+     template: ` <div>
+                   <slot v-bind:compText='comp2c'></slot>
+                   <slot></slot>
+                   <slot name='slot1'></slot>
+                   <p>{{comp2a}}</p>
+                   <p>{{comp2c}}</p>
+                   <slot name='slot2'></slot>  
+                   <slot  v-bind:comp3cb='comp3c'></slot>
+                                          
+                  </div>
+            `,
+     props: ['valor1'],
+
+     data: function() {
+         return {
+             comp2a: "vim do componente, sou 2a",
+             comp2b: "vim do component, sou 2b",
+             comp2c: "sou do component, my name is 2c",
+             comp3c: "sou do component, sou O 2C"
+
+         }
+     }
  })
 
  var app = new Vue({
      el: '#app1',
      data: {
+
+         question: "",
+         answer: " vamos la, pergunte algo? ",
+         valor: 1,
+         resultado: "ola",
+         comp2a: "vim do elemento pai, sou 2a",
+         comp2b: "vim do elemento pai, sou 2b",
+         comp2c: "vim do elemento pai, sou 2c",
+         comp3c: "vim do elemento pai, sou 2c",
+
          teste4: [
              { key: 0, value: "value1" },
              { key: 1, value: "value2" },
@@ -45,10 +111,48 @@
 
      },
 
+     watch: {
+
+         question: function() {
+
+             this.answer = "estou pesquisando..."
+             setTimeout(() => { this.answer = "isso eu nãoo sei" }, 2000)
+         }
+     },
+
      methods: {
+         debounce: function(fn, delay) {
+             timer = null
+             return function(fn, delay) {
+                 clearTimeout(timer);
+                 setTimeout(fn, delay);
+             }
+
+         },
+
+         result: function(n) {
+             setTimeout(() => {
+                 a = n + " " + "acrescimo"
+                 alert(a);
+             }, 1000);
+
+         },
          estado: function() { this.teste = !this.teste; },
-         fteste2: function() { this.teste2 = !this.teste2; }
+         fteste2: function() {
+             this.teste2 = !this.teste2;
+
+         },
+
+
+
+
      }
 
 
+ })
+
+ // usando o emit e os escutadores on e once, parece que sóe scutam os eventos emitidos pela função emmit
+
+ app.$on('click', function() {
+     return app.valor += 10
  })
